ae01b52c2e0e18d33f9a9e5ee8e5a3cf8e4f2e03
Mike Sinnema <msinnema33@gmail.com>
2020-01-23 13:39:59 -0700

    added bootstrap reactstrap styled-components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import HeaderInfo from "./Components/Head/Header.js";
 import FooterInfo from './Components/Foot/Footer.js';
+import styled from 'styled-components';
 
 import "./App.css";
+
+const description = styled.p`
+  width: 90
+
+
+
+`;
 
 function App() {
   const [pic, setPic] = useState([]);  
@@ -26,11 +34,7 @@ function App() {
      <HeaderInfo /> 
       <p>Description:  {pic.explanation}</p>
       <img src={pic.url}/>
-      <p>
-        {/* Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀! */}
-      </p>
-
+      
       <FooterInfo />
     </div>
   );
--- src/Components/Head/Header.js
+++ src/Components/Head/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import logo from './Components/favicon-192.png';
+import styled from 'styled-components';
 
 
 const HeaderInfo = props => {
